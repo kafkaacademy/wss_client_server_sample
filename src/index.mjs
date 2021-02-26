34cd97263213fa878fcb6d02c0c0ac9e12dc3275
Herbrand Hofker <herbrandhofker@users.noreply.github.com>
2021-02-26 16:36:54 +0100

    shorter

--- src/index.mjs
+++ src/index.mjs
@@ -2,20 +2,22 @@ const port = process.env.PORT
 const url = `wss://localhost:${port}`;
 
 const error = document.body.appendChild(document.createElement('div'));
+error.innerText='no error'
 const data = document.body.appendChild(document.createElement('div'));
-data.id = "data"
+data.innerText="message from server: "
+
 try {
   const socket = new WebSocket(url);
 
   // Connection opened
-  socket.addEventListener('open', (event)=> {
+  socket.onopen= ()=>{
     socket.send('Hello from client!');
-  });
+  };
 
   // Listen for messages
-  socket.addEventListener('message',(event) =>{
-    data.innerText = 'Message from server : ' + event.data;
-  })
+  socket.onmessage=(event) =>{
+    data.innerText +=  event.data;
+  }
 } 
 catch (e) {
   error.innerText="error" + e;
