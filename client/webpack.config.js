fc8eef4579664b0b56c7ed2b07910f4f505bb12c
Herbrand Hofker <herbrandhofker@users.noreply.github.com>
2021-03-10 03:58:29 +0100

    melding url added

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
 
     plugins: [
         new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
-        new HtmlWebpackPlugin({ title: 'Auth0 LoginButton Demo' }),       
+        new HtmlWebpackPlugin({ title: 'https/wss demo' }),       
         new Dotenv()
     ],
     output: {
